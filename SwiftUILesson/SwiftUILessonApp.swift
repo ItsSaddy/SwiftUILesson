adfb085d0216d663c64636ef2a276dd8f0e03a45
Saddam <leetsaddam@mail.ru>
2024-06-06 14:42:05 +0500

    Initial Commit

---
+++ SwiftUILesson/SwiftUILessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILessonApp.swift
+//  SwiftUILesson
+//
+//  Created by Boboev Saddam on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
