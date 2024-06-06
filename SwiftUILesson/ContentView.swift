e19e888039d24f9371f62255cfda0b12898731ea
Saddam <leetsaddam@mail.ru>
2024-06-06 15:39:38 +0500

    feat: first working with SwiftUI

--- SwiftUILesson/ContentView.swift
+++ SwiftUILesson/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedLightIndex: Int?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Circle()
+                    .foregroundStyle(.red.opacity(selectedLightIndex == 0 ? 1 : 0.5))
+                    .frame(width: 125)
+                    .overlay(Circle().stroke(Color.black, lineWidth: 6))
+                
+                Circle()
+                    .foregroundStyle(.yellow.opacity(selectedLightIndex == 1 ? 1 : 0.5))
+                    .frame(width: 125)
+                    .overlay(Circle().stroke(Color.black, lineWidth: 6))
+                
+                Circle()
+                    .foregroundStyle(.green.opacity(selectedLightIndex == 2 ? 1 : 0.5))
+                    .frame(width: 125)
+                    .overlay(Circle().stroke(Color.black, lineWidth: 6))
+            }
+            .padding()
+            Spacer()
+        }
+        
+        Button(selectedLightIndex == nil ? "Start" : "Next") {
+            guard var selectedLightIndex = selectedLightIndex else {
+                selectedLightIndex = 0
+                return
+            }
+            
+            selectedLightIndex = (selectedLightIndex + 1) % 3
+            
+            self.selectedLightIndex = selectedLightIndex
         }
         .padding()
+        .font(.largeTitle)
     }
 }
 
